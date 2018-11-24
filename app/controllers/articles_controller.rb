21fa2764381a2475635f2a15af74172f203b38ab
shahin1290 <s.patowary@yahoo.com>
2018-11-25 00:19:03 +0100

    Adds edit and update action in controllers, adds edit page in views and all test pass

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,9 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+  def edit
+    @article = Article.find(params[:id])
+  end
  
   def create
     @article = Article.new(article_params)
@@ -12,6 +15,17 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+
+  def update
+    @article = Article.find(params[:id])
+ 
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @article = Article.find(params[:id])
   end
