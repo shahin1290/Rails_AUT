5856d6677cdeed5e778ba6f572093b69685c0156
shahin1290 <s.patowary@yahoo.com>
2018-11-25 15:19:11 +0100

    Adds validation to comments that fields can not be blank

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
+    validates :user_email, presence: true
+    validates :content, presence: true
     belongs_to :article
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -11,10 +11,4 @@ RSpec.describe Comment, type: :model do
     it { is_expected.to validate_presence_of :content } 
   end
 
-  describe 'Factory' do
-    it 'should have valid Factory' do
-      expect(FactoryBot.create(:comment)).to be_valid 
-    end
-  end
-
 end
