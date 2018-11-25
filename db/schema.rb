f6d761f062aeb26560c645c770d3fbca28659083
shahin1290 <s.patowary@yahoo.com>
2018-11-25 12:26:16 +0100

    Generates migration for adding article id to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_25_093507) do
+ActiveRecord::Schema.define(version: 2018_11_25_101827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2018_11_25_093507) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "article_id"
   end
 
 end
